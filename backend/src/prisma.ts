cb730c05fed2dc5afe2545c1d585dfb30ea70989
Scott Dunning <swdunning@me.com>
2026-03-02 22:03:47 -0700

    remove prisma.config.js and add to git ignore

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,13 +1,12 @@
 import "./config/env"
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
 
 const connectionString = process.env.DATABASE_URL
 if (!connectionString) {
 	throw new Error("DATABASE_URL is missing in backend/.env")
 }
 
-const adapter = new PrismaPg({ connectionString })
+//const adapter = new PrismaPg({ connectionString })
 
 //export const prisma = new PrismaClient({ adapter })
 export const prisma = new PrismaClient()
